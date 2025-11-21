b75900bbd644521e5f70668a8819335e322df52e
Rene Matin Kuda <39137812+renekuda38@users.noreply.github.com>
2025-11-21 07:22:00 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,28 +1,29 @@
 # backend/database.py
 # postgres db connection
 
-import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import os # pristup k env vars
+import psycopg2 # postresql driver
+from psycopg2.extras import RealDictCursor # vraci dict namiesto tuple
 
 
 def get_db_connection():
-    database_url = os.getenv("DATABASE_URL")
+    database_url = os.getenv("DATABASE_URL") # z docker-compose env
 
     if not database_url:
         raise ValueError("DATABASE_URL environment variable is not set")
+        # app okamzite zastane s jasnym errorom
 
-    conn = psycopg2.connect(database_url, cursor_factory = RealDictCursor)
+    conn = psycopg2.connect(database_url, cursor_factory = RealDictCursor) # dict output
 
     return conn
 
 def test_db_connection():
     try:
         conn = get_db_connection()
-        cur = conn.cursor()
+        cur = conn.cursor() # kurzor pre dopyty
 
-        cur.execute("SELECT 1")
-        cur.fetchone()
+        cur.execute("SELECT 1") # minimal test
+        cur.fetchone() # python dostane data(riadok) od DB
 
         cur.close()
         conn.close()
